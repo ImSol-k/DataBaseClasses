00537c96453e70bfaa9107ffb3a09f2dac379582
tiq0015 <tiq0015@naver.com>
2024-01-22 19:50:17 +0900

    정리

--- Practice/Practice01.sql
+++ Practice/Practice01.sql
@@ -1,6 +1,6 @@
 select * from employees;
 select * from jobs;
-
+##################
 begin; #문제 1
 -- 전체 직원의 다음 정보 조회
 -- 입사일 올림차순으로 정렬
@@ -15,6 +15,7 @@ order by hire_date asc,
          first_name asc;
 end;
 
+##################
 begin; #문제 2
 -- 업무별로 업무이름과 최고월급을 월급의 내림차순으로 정렬
 
@@ -24,6 +25,7 @@ from jobs
 order by max_salary desc;
 end;
 
+##################
 begin; #문제 3
 -- 담당매니저가있지만 커미션비율이 없고, 월급이 3000초과인
 -- 직원의 이름, 매니저아이디, 커미션비율, 월급출력(월급 내림차순)
@@ -39,6 +41,7 @@ and salary > 3000;
 
 end;
 
+##################
 begin; #문제 4
 -- 최고월급이 10000이상인 업무의 이름과 최고월급을
 -- 최고월급의 내림차순으로 출력 
@@ -50,6 +53,7 @@ where max_salary >= 10000
 order by max_salary desc;
 end;
 
+##################
 begin; #문제 5
 -- 월급이 14000미만 10000이상인 직원의 이름(first_name), 월급, 커미션퍼센트를
 -- 월급 내림차순으로 출력(단 커미션퍼센트가 null이면 0으로 나타내기) 
@@ -62,6 +66,7 @@ where salary between 10000 and 14000
 order by salary desc;
 end;
 
+##################
 begin; #문제 6
 -- 부서번호가 10, 90, 100인 직원의 이름, 월급, 입사일, 부서번호출력
 -- 입사일은 1977-12 과 같이 표시
@@ -76,6 +81,7 @@ or department_id = 90
 or department_id = 100;
 end;
 
+##################
 begin; #문제 7
 -- ,dlfmadp S또는 s가 들어가는 직원의 이름, 월급출력 
 
@@ -85,6 +91,7 @@ from employees
 where first_name like '%s%';
 end;
 
+##################
 begin; #문제 8
 -- 부서이름이 긴 순서대로 전체부서 출력
 
@@ -93,6 +100,7 @@ from departments
 order by char_length(department_name) desc;
 end;
 
+##################
 begin; #문제 9
 -- 지사가 있을것으로 예상되는 나라의 이름을 대문자로 출력(올림차순)
 
@@ -101,6 +109,7 @@ from countries
 order by country_name asc;
 end;
 
+##################
 begin; #문제 10
 -- 입사일이 03/12/31 이전 입사한 직원의 이름, 월급, 전화번호, 입사일 출력
 -- 전화번호는 000-000-0000형태로 출력 
