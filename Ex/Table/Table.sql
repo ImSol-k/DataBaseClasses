977becfab3ca46b729103b92dfa7cdbac3c1332e
tiq0015 <tiq0015@naver.com>
2024-01-26 15:55:13 +0900

    .

--- Ex/Table/Table.sql
+++ Ex/Table/Table.sql
@@ -12,3 +12,23 @@ create table book(
     author_id	int,
     constraint book_fk foreign key (author_id)
     references author (author_id));
+-- 작가 등록 ---------------------------------
+select * from author;
+-- 묵시적 방법
+-- 테이블 생성시 정의한 순서에 따라 값 지정
+insert into author values (5,'이준기','세계최강');
+insert into author values (3, '김소리', null);
+-- 명시적 방법
+-- 지정되지 않은 컬럼은 null자동입력
+insert into author(author_id, author_name) 
+values (4,'정우성');
+-- 레코드 변경 --------------------------------
+update author 
+set author_name = '기안84',
+	author_desc = '웹툰작가'
+where author_id = 1;
+-- 일부만 변경 가능
+update author 
+set author_name = '김도현'
+where author_id = 3;
+
