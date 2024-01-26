d03058ab8a36b8b6c54dbebb3cdf127413cd41af
tiq0015 <tiq0015@naver.com>
2024-01-26 13:14:41 +0900

    CreateTable

---
+++ Ex/CreateTable.sql
@@ -0,0 +1,21 @@
+-- 테이블 생성 ---------------------------------
+create table book(
+	book_id		int,
+    title		varchar(50),
+    author 		varchar(20),
+    pub_date 	datetime
+); 
+show tables;
+select * from book;
+-- 컬럼 수정 -----------------------------------
+-- 컬럼 추가
+alter table book add pubs varchar(50);
+-- 컬럼 데이터변경
+alter table book modify title varchar(100);
+-- 컬럼명 변경
+alter table book rename column title to subject;
+-- 컬럼삭제 
+alter table book drop author;
+-- 테이블 수정 ---------------------------------
+rename table book to article;
+select * from article;
--- Ex/chapter05.sql
+++ Ex/chapter05.sql
@@ -7,12 +7,13 @@ flush privileges; #생략가능
 grant all privileges on webdb.* to 'webdb'@'%';
 -- 계정삭제 -----------------------------------
 drop user 'webdb'@'%';
--- web_db 생성 삭제 ---------------------------
+-- DB 생성 -----------------------------------
 create database web_db
 	default character set utf8mb4
     collate utf8mb4_general_ci
     default encryption = 'n';
 show databases;
+-- DB 삭제 -----------------------------------
 drop database web_db;
 -- book생성 ----------------------------------
 drop user if exists 'book'@'%'; #계정이 있다면 삭제
