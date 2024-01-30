85f5157049a6804f1dd7cd86522a02d2554e237f
tiq0015 <tiq0015@naver.com>
2024-01-30 12:21:28 +0900

    수정

--- src/com/javaex/ex01/BookApp.java
+++ src/com/javaex/ex01/BookApp.java
@@ -1,6 +1,5 @@
 package com.javaex.ex01;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,9 +10,6 @@ public class BookApp {
 		AuthorDao author = new AuthorDao();
 		List<AuthorVo> authorList = author.authorList();
 		Scanner in = new Scanner(System.in);
-		String authorName, authorDesc;
-		int num;
-		boolean start = true;
 		
 		for(AuthorVo vo : authorList) {
 			int id = vo.getId();
@@ -22,49 +18,7 @@ public class BookApp {
 			System.out.println(id + ". " + name + " / " + desc);
 		}
 
-		/*
-		author.authorSelect();
-		while (start) {
-			System.out.println("==================================================");
-			System.out.println("1.리스트 2.추가 3.삭제 4.수정 5.종료");
-			System.out.print(">> ");
-			num = in.nextInt();
-			in.nextLine();
-			System.out.println("==================================================");
-			switch (num) {
-			case 1:
-				author.authorShowList();
-				System.out.println("==================================================");
-				break;
-			case 2:
-				System.out.print("작가입력 >>");
-				authorName = in.nextLine();
-				System.out.print("작가정보 입력 >> ");
-				authorDesc = in.nextLine();
-				author.AuthorInsert(authorName, authorDesc);
-				break;
-			case 3:
-				System.out.print("삭제할 번호 입력 >> ");
-				num = in.nextInt();
-				author.authorDelete(num);
-				break;
-			case 4:
-				System.out.print("수정할 id입력 >> ");
-				num = in.nextInt();
-				in.nextLine();
-				System.out.print("작가수정 >>");
-				authorName = in.nextLine();
-				System.out.print("작가정보 수정 >> ");
-				authorDesc = in.nextLine();
-				author.authorUpdate(authorName, authorDesc, num);
-				break;
-			case 5:
-				System.out.println(">> 종료되었습니다 <<");
-				start = false;
-				break;
-
-			}
-		}//while*/
+		
 		in.close();
 
 	}//main
